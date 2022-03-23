2c9395514a7f22267eed5b72e120a837a58fb1a7
Denis <wolf-den@tut.by>
2022-03-23 22:10:00 +0300

    feat: create interfaces

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchComponent } from './search/search.component';
+import { SearchResultComponent } from './search/search-result/search-result.component';
+import { SearchItemComponent } from './search/search-item/search-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    SearchComponent,
+    SearchResultComponent,
+    SearchItemComponent,
   ],
   imports: [
     BrowserModule,
