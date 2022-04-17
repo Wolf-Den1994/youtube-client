70b7e1b3f5aeda3eaf59d0f6eb1f736e0ae7e7a0
Denis <wolf-den@tut.by>
2022-04-17 19:46:28 +0300

    feat: add subscription auth

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { YoutubeService } from '../../youtube/services/youtube.service';
+import { AuthService } from '../../auth/services/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -7,7 +9,15 @@ import { YoutubeService } from '../../youtube/services/youtube.service';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  private subscription: Subscription;
+
   constructor(
+    private authService: AuthService,
     public youtubeService: YoutubeService,
-  ) {}
+  ) {
+    this.subscription = this.authService.authState$.subscribe((value) => {
+      if (value) this.youtubeService.showHeader();
+      else this.youtubeService.hideHeader();
+    });
+  }
 }
